4e28e1dfdbe14c20356f62b07c5acadde7a5450d
wwyattbrendle <wyatt@wyattbrendle.tech>
2023-04-22 15:21:49 -0400

    Corrected error in winner calculation Revamped playGame() make game best 2 out of 3

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const validChoices = ["rock", "paper", "scissors"];
-
+const scoreBoard = [0, 0];
 const validityChecker = function(input) {
     if (!validChoices.includes(input)) {
         alert("Invalid input, retry");
@@ -23,15 +23,32 @@ const computerSelection = function() {
 const playGame = function(){
     const computerChoice = computerSelection();
     const playerChoice = playerSelection();
+    let hasWinner = false;
     if (playerChoice === validChoices[computerChoice]){
         alert("It's a Tie!  Play Again");
         playGame();
         return;
-    } else if(validChoices[computerChoice + 1] % 3 === playerChoice){
-        alert("Player Wins")
+    } else if(validChoices[(computerChoice + 1) % 3] === playerChoice){
+        scoreBoard[1]++;
+        alert(`player wins.  scoreBoard[1] = ${scoreBoard[1]}`);
     } else {
-        alert("Computer Wins")
+        scoreBoard[0]++;
+        alert(`computer wins.  scoreBoard[0] = ${scoreBoard[0]}`);
+    }
+    if(scoreBoard.includes(2)){
+        hasWinner = true;
+        if(scoreBoard[0] == 2){
+            console.log("Computer Wins!");
+        } else {
+            console.log("Player Wins!");
+        }
     }
     console.log(`player choice: ${playerChoice}, computer choice: ${validChoices[computerChoice]}`);
+    console.log(`player score: ${scoreBoard[1]}, computer score: ${scoreBoard[0]}`)
+    if (!hasWinner){
+        playGame();
+    }
+    return;
 }
+
 playGame();
